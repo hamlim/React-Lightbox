e3004eebc653ecaf9786fb9913669075b8626379
Matt Hamlin <hamlim@outlook.com>
2017-12-30 10:22:16 -0800

    Cleaning up code a bit, better patterns, some more docs

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
 # React Lightbox
 
-This is a simple Lightbox component for React.
+This is a simple Lightbox component for React (v16 and above).
 
 ### To use in your project:
 
-```
-import {Lightbox} from '/path/to/Lightbox.js';
+```javascript
+import Lightbox from '/path/to/Lightbox.js';
 
 require('path/to/Lightbox.css');
-```
 
+...
+
+render(
+  <Lightbox
+    images={
+      [
+        {
+          img: 'path/to/image.jpg',
+          caption: 'Image Caption'
+        },
+        {
+          img: 'path/to/another_image.jpg',
+          caption: 'Another Image Caption'
+        }
+      ]
+    }
+  />,
+  root
+);
+```
 
 ### Requirements:
 
-* Webpack
 * React
 * CSSNext (color function, this can be changed out to whatever color you want however)
---
+++ src/Lightbox.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react'
+
+const Lightbox = props => {
+  return (
+    <section className="LB-component">
+      <h2 className="LB-title">{props.title}</h2>
+      {props.images.map((obj, index) => (
+        <Fragment>
+          <div className="LB-wrapper" key={index}>
+            <a href={'#LB_target_' + index} className="escape">
+              <img id={'LB_' + index} src={obj.img} alt={obj.caption} className="LB-image" />
+            </a>
+            <small className="LB-caption">{obj.caption}</small>
+          </div>
+          <a href={'#LB_' + index} className="LB-target-wrapper escape" id={'LB_target_' + index} key={index}>
+            <div>
+              <img src={obj.img} alt={obj.caption} className="LB-target-img" />
+              <small className="LB-target-caption">{obj.caption}</small>
+            </div>
+          </a>
+        </Fragment>
+      ))}
+    </section>
+  )
+}
+export default Lightbox
