36c8f9dbc57ce9f18b172dfefee7b5845c0a0b6b
Matt Hamlin <hamlim@outlook.com>
2018-01-14 14:05:50 -0500

    Removing Fragment syntax

--- src/Lightbox.js
+++ src/Lightbox.js
@@ -1,32 +1,22 @@
 import React from 'react'
 
-const Fragment =
-  React.Fragment ||
-  function({ children }) {
-    return children
-  }
-
-const Lightbox = props => {
-  return (
-    <section className="LB-component">
-      <h2 className="LB-title">{props.title}</h2>
-      {props.images.map((obj, index) => (
-        <Fragment>
-          <div className="LB-wrapper" key={index}>
-            <a href={'#LB_target_' + index} className="escape">
-              <img id={'LB_' + index} src={obj.img} alt={obj.caption} className="LB-image" />
-            </a>
-            <small className="LB-caption">{obj.caption}</small>
-          </div>
-          <a href={'#LB_' + index} className="LB-target-wrapper escape" id={'LB_target_' + index} key={index}>
-            <div>
-              <img src={obj.img} alt={obj.caption} className="LB-target-img" />
-              <small className="LB-target-caption">{obj.caption}</small>
-            </div>
-          </a>
-        </Fragment>
-      ))}
-    </section>
-  )
-}
+const Lightbox = props => (
+  <section className="LB-component">
+    <h2 className="LB-title">{props.title}</h2>
+    {props.images.map((obj, index) => [
+      <div key="image" className="LB-wrapper">
+        <a href={'#LB_target_' + index} className="escape">
+          <img id={'LB_' + index} src={obj.img} alt={obj.caption} className="LB-image" />
+        </a>
+        <small className="LB-caption">{obj.caption}</small>
+      </div>,
+      <a key="link" href={'#LB_' + index} className="LB-target-wrapper escape" id={'LB_target_' + index}>
+        <div>
+          <img src={obj.img} alt={obj.caption} className="LB-target-img" />
+          <small className="LB-target-caption">{obj.caption}</small>
+        </div>
+      </a>,
+    ])}
+  </section>
+)
 export default Lightbox
